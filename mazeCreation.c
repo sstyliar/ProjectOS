0ad9f51bfbd464c235a3212fb162236078ca076c
sstyliar <37962776+sstyliar@users.noreply.github.com>
2018-05-14 05:23:33 +0300

    git Rich, git B*tches

--- mazeCreation.c
+++ mazeCreation.c
@@ -3,23 +3,19 @@
 #include <time.h>
 #include <unistd.h>
 
-#define human_face  "👲"
-#define monster_face  "👻"
-#define boss_face  "👹"
-
-typedef struct {
-    int date;
-    int month;
-    int year;
-} OfferDate;
-
-
-typedef struct {
-char* type;
-char* destination;
-float price;
-OfferDate date; 
-} Offer;
+// typedef struct {     //template
+//     int date;     
+//     int month;    
+//     int year;     
+// } OfferDate;      
+                     
+                     
+// typedef struct {     
+// char* type;          
+// char* destination;    
+// float price;      
+// OfferDate date;      
+// } Offer;             //template
 
 
 //***General Notes***
@@ -109,41 +105,49 @@ OfferDate date;
 
 
 
- // Display the maze. we want this one
-void ShowMaze(const char *maze, int width, int height) {
-   int x, y;
+ // Display-save the maze. we want this one
+void Show_saveMaze(const char *maze, int width, int height, int file_num) {
+   int i, x, y;
 
-   for(y = 0; y < height; y++) {
-      for(x = 0; x < width; x++) {
-
-         printf("maze[%d][%d] = %d  ",y,x,maze[y*width+x]);
-      }
-      printf("\n");
+   FILE *files[10];
+   char filename[20];
+   sprintf(filename, "level%d.txt", file_num);
+   files[i] = fopen(filename, "w");
+   if (files[i] == NULL)
+   {
+      printf("Error opening file!\n");
+      exit(1);
    }
 
    for(y = 0; y < height; y++) {
       for(x = 0; x < width; x++) {
          switch(maze[y * width + x]) {
-         case 1:  printf("{||}");  break;
-         case 2:  printf("%c",*human_face);  break;
-         default: printf("    ");  break; 
+         case 1:  printf("{||}"); fprintf(files[i],"{||}"); break;
+         case 2:  printf(" 👲  "); fprintf(files[i]," 👲   "); break;
+         case 3:  printf(" 👻  "); fprintf(files[i]," 👻   "); break;
+         case 4:  printf(" 👹  "); fprintf(files[i]," 👹   "); break;
+         case 5:  printf(" 💰  "); fprintf(files[i]," 💰   "); break;
+         default: printf("    "); fprintf(files[i],"    "); break; 
          }
          
       }
-      printf("\n");
+      printf("\n"); fprintf(files[i], "\n");
    }
+   printf("\n");
 }
 
-  // Carve the maze starting at x, y.  we want this one 
+  // Anoigei to monopati
 void CarveMaze(char *maze, int width, int height, int x, int y) {
 
    int x1, y1;    
    int x2, y2;
    int dx, dy;
    int dir, count;
+   int flag = 0;
+   int phantom_pos, loot_pos;
+
 
    dir = rand() % 4;
-   // printf("%d", dir);
    count = 0;
    while(count < 4) {
       dx = dy = 0;
@@ -153,54 +157,57 @@ void CarveMaze(char *maze, int width, int height, int x, int y) {
       case 2:  dx = -1; break;
       default: dy = -1; break;
       }
-      x1 = x + dx;
-      y1 = y + dy;
-      x2 = x1 + dx;
-      y2 = y1 + dy;
+      x1 = x + dx;      //
+      y1 = y + dy;      //
+      x2 = x1 + dx;     //
+      y2 = y1 + dy;     //
       
       if(x2 > 0 && x2 < width && y2 > 0 && y2 < height && maze[y1 * width + x1] == 1 && maze[y2 * width + x2] == 1) {
-         printf("\tx1 = %d, x2 = %d, y1 = %d, y2 = %d\n", x1, x2, y1, y2);
          maze[y1 * width + x1] = 0;
-         printf("maze cell number = maze[%d] is equal to 0\n", y1 * width + x1);
          maze[y2 * width + x2] = 0;
-         printf("maze cell number = maze[%d] is equal to 0\n", y2 * width + x2);
          x = x2; y = y2;
          dir = rand() % 4;
          count = 0;
+         flag = 0;
+         if (flag ==0 )
+         {
+            phantom_pos = y1 * width + x1;
+            loot_pos = y2 * width + x2;
+         }
       } else {
          dir = (dir + 1) % 4;  //auksanei to dir alla to krataei panta <4
          count += 1;
+         flag++;
       }
    }
-
+   maze[phantom_pos] = 3;
+   maze[loot_pos] = 5;
 }
 
  // Generate maze in matrix maze with size width, height.
-  // MOST DEF WE WANT THIS ONE 
 void GenerateMaze(char *maze, int width, int height) {
-
-   int x, y;
-
-   /* Initialize the maze. */
-   for(x = 0; x < width * height; x++) {
-      maze[x] = 1;
-   }
-   maze[1 * width + 1] = 0;
    
-   /* Seed the random number generator. */
-   srand(time(0));
-
-   /* Carve the maze. */
-   for(y = 1; y < height; y += 2) {
-      for(x = 1; x < width; x += 2) {
-         CarveMaze(maze, width, height, x, y);
+      int x, y;
+      /* Initialize the maze. */
+      for(x = 0; x < width * height; x++) {
+         maze[x] = 1;
       }
-   }
-
-   /* Set up the entry and exit. */
-   maze[0 * width + 1] = 0;
-   maze[(height - 1) * width + (width - 2)] = 0;
+      maze[1 * width + 1] = 0;  //to amesws epomeno keli einai open, gia na exei lush 
+      
+      /* Seed the random number generator. */
+      srand(time(0));
    
+      /* Carve the maze. */
+      for(y = 1; y < height; y += 2) {       //y+=2 gia na exei treiades gia ton elegxo
+         for(x = 1; x < width; x += 2) {     
+            CarveMaze(maze, width, height, x, y);
+         }
+      }
+   
+      //default eisodos k eksodos apo to lavurintho, maze[0][1] kai maze[..][..]
+      maze[0 * width + 1] = 2;
+      maze[(height - 1) * width + (width - 2)] = 4;
+
 }
 
 
@@ -212,36 +219,33 @@ int main(int argc,char *argv[]) {
    
 
    int width, height;
+   int file_num=1;
    char *maze;
 
-   if(argc != 3 && argc != 4) {
-      // printf("usage: maze <width> <height> [s]\n");
-      exit(EXIT_FAILURE);
-   }
+   // if(argc != 3 && argc != 4) {
+   //    printf("usage: maze <width> <height> [s]\n");
+   //    exit(EXIT_FAILURE);
+   // }  
 
-   /* Get and validate the size. */
-   width = atoi(argv[1]) * 2 + 3;
-   height = atoi(argv[2]) * 2 + 3;
-   if(width < 7 || height < 7) {
-      // printf("error: illegal maze size\n");
-      exit(EXIT_FAILURE);
-   }
+   // /* Get and validate the size. */
+   // width = atoi(argv[1]);
+   // height = atoi(argv[2]);
+   // if(width < 7 || height < 7) {
+   //    printf("error: illegal maze size\n");
+   //    exit(EXIT_FAILURE);
+   // }
    
-   /* Allocate the maze array. */
-   maze = (char*)malloc(width * height * sizeof(char));
-   if(maze == NULL) {
-      // printf("error: not enough memory\n");
+   for (int i = 7; i < 15; i+=2)
+   {  
+      maze = (char*)malloc(i * i * sizeof(char));
+      if(maze == NULL) {
+      printf("error: not enough memory\n");
       exit(EXIT_FAILURE);
+      }
+      GenerateMaze(maze,i,i);
+      Show_saveMaze(maze,i,i,file_num);
+      free(maze);
+      file_num++;
    }
-
-   /* Generate and display the maze. */
-   // GenerateMaze(maze, width, height);
-   // ShowMaze(maze, width, height, h1);
-
-   GenerateMaze(maze, 9, 9);
-   ShowMaze(maze, 9, 9);
-   
-   free(maze);
    return 0;
 }
-
