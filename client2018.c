015384d95fbc4bfa3e2c02651d09438f5633ec87
vfantidou <39241875+vfantidou@users.noreply.github.com>
2018-05-21 20:36:53 +0300

    Add files via upload

--- client2018.c
+++ client2018.c
@@ -9,45 +9,73 @@
 
 #define SIZE 1024
 
+typedef struct 
+{
+   int health;
+   int armor;
+   int attack;
+   float accurasy;
+   int level;
+
+}human_stats;
+
+
+
 int main(int argc, char *argv[])
 {
 	int sock,i;
-	char msg[SIZE];
+	int pass_temp = 1234;
+	int sock1;
+	human_stats h1;
+	int sphlia[50];
+	char username[30]="Vasiliki";
+	char password[30];
+	sprintf(password, "%d", pass_temp);
 	struct sockaddr_in addr;//Internet socket
 	
 	addr.sin_family = AF_INET;
 	inet_aton(argv[1],&addr.sin_addr);
 	addr.sin_port = htons(atoi(argv[2]));
+
 	sock = socket(AF_INET,SOCK_STREAM,0);//Stream socket
+	sock1 = socket(AF_INET,SOCK_STREAM,0);//Stream socket
 	connect(sock,(struct sockaddr *)&addr,sizeof(addr));
+	connect(sock1,(struct sockaddr *)&addr,sizeof(addr));
+	sphlia[0]=0;
 	
-	strcpy(msg,"");
-	for(i=3;i<argc;i++){
-		strcat(msg,argv[i]);
-		strcat(msg," ");
+
+	for(i=0;i<50;i++){
+	
+		sphlia[i] = sphlia[0]++;
+		printf("sending  : %d\n",sphlia[i]);
+		send(sock,&sphlia,SIZE,0); 
+		//sendto(sock,&msg,strlen(msg)+1,0,NULL,0);
 	}
-	msg[strlen(msg)-1]='\0'; //Remove the last space from string
 	
-	printf("sending command : %s\n",msg);
+	send(sock1,&username,SIZE,0);
+	send(sock1,&password,SIZE,0);
 	
-	send(sock,msg,strlen(msg)+1,0);
-	//sendto(sock,msg,strlen(msg)+1,0,NULL,0);
+	printf("\nusername: %s\n",username);
+	printf("\npassword: %s\n",password);
+	
+	h1.health=100;
+	h1.armor=10;
+	h1.attack=50;
+	h1.accurasy=20;
+	h1.level=1;	
+	send(sock1,&h1.health,SIZE,0);
+	send(sock1,&h1.armor,SIZE,0);
+	send(sock1,&h1.attack,SIZE,0);
+	send(sock1,&h1.accurasy,SIZE,0);
+	send(sock1,&h1.level,SIZE,0);	
+
+	printf("%d\n", h1.health);
+	printf("%d\n", h1.armor);
+	printf("%d\n", h1.attack);
+	printf("%f\n", h1.accurasy);
+	printf("%d\n", h1.level);
+
+	close(sock1);
 	close(sock);
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- server2018.c
+++ server2018.c
@@ -5,13 +5,29 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define SIZE 1024
 
+
+typedef struct 
+{
+   int health;
+   int armor;
+   int attack;
+   float accurasy;
+   int level;
+
+}human_stats;
+
 int main(int argc,char *argv[])
 {
-	int psock,sock;
-	char msg[SIZE];
+	int psock,sock,i;
+	int sock1;
+	human_stats h1;	
+	int sphlia[50];
+	char username[30];
+	char password[30];
 	struct sockaddr_in addr; //internet socket
 	
 	addr.sin_family = AF_INET;
@@ -21,49 +37,52 @@ int main(int argc,char *argv[])
 	bind(psock,(struct sockaddr *)&addr,sizeof(addr));
 	listen(psock,3);//dexetai 3 paixtes client 
 
-	strcpy(msg,"");
-	while (strcmp(msg,"exit") !=0) {		//an to msg einai exit kleinei thn syndesh o server
-		printf("\nwaiting for new connection (port: %s)...\n",argv[1]);
+	
+	printf("\n waiting for new connection (port: %s)...\n",argv[1]);
+	sock = accept(psock,NULL,NULL);
+	sock1 = accept(psock,NULL,NULL);
 		
-		sock = accept(psock,NULL,NULL);
+	printf("connection established...\n");
+	//for(i=0;i<50;i++){
+	//	sphlia[i]=0;
+	//}
+	
+	for(i=0;i<50;i++){
 		
-		printf("connection established...\n");
-		
-		recv(sock, msg,SIZE,0);
+		recv(sock,sphlia,SIZE,0);
 		//recfrom(sock,msg,SIZE,0,NULL,NULL);
-
-		if (strcmp(msg,"exit") !=0){
-			printf("running command:[%s]\n",msg);
-			system(msg);
-		}
-		close(sock);
-		printf("\nconnect closed...\n");
+		printf("received:%d\n",sphlia[i]);
+		
 	}
+	close(sock);
+
+	recv(sock1,username,SIZE,0);
+	recv(sock1,password,SIZE,0);
+	
+	printf("\nusername: %s\n",username);
+	printf("\npassword: %s\n",password);
+
+	recv(sock1,&h1.health,SIZE,0);
+	recv(sock1,&h1.armor,SIZE,0);
+	recv(sock1,&h1.attack,SIZE,0);
+	recv(sock1,&h1.accurasy,SIZE,0);
+	recv(sock1,&h1.level,SIZE,0);
+
+	printf("%d\n", h1.health);
+	printf("%d\n", h1.armor);
+	printf("%d\n", h1.attack);
+	printf("%f\n", h1.accurasy);
+	printf("%d\n", h1.level);
+
+	printf("\n connection closed...\n");
+	close(sock1);
 	close(psock);
 	unlink(argv[1]);
+	
+	
+	
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
